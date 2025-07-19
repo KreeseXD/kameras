8ae6daad47b4f0ace4cea601b45a6d2d78404678
Christopher Nguyen <chris_ngu@pm.me>
2025-07-19 00:58:47 -0500

    adding in custom tool class

---
+++ kameras/Kamera.cs
@@ -0,0 +1,51 @@
+using System.Xml.Serialization;
+using StardewValley;
+
+namespace Kamera;
+
+
+// used StardewValley.Tools.FishingRod as a refernce
+// https://stardewvalleywiki.com/Modding:Tools
+public class Kamera : StardewValley.Tools.GenericTool
+{
+
+    public const int FilmIndex = 0;
+    public const int LensIndex = 0;
+
+    // 36 exposures, but can move this to a yml file for half frame rolls/ 24 frame
+    public static int maxFilmUses = 36;
+
+    public bool isTakingPhoto;
+
+        public Kamera()
+    {
+        // wtf is this even doing
+        initNetFields();
+        base.Category = -99;
+
+        Name = "kamera";
+        SetSpriteIndex(189);
+        IndexOfMenuItemView = 8;
+        AttachmentSlotsCount = Math.Max(0, 2);
+        base.Category = -99;
+        UpgradeLevel = 0;
+        // stackable = false;
+    } 
+
+
+
+    // StardewValley.Tools.FishingRod
+    // gonna need to change this
+    // will need to reference fishing rod to see how it's using attachments
+    // need attachments for film type and lens
+    // public Camera()
+    //     : base("Camera", 0, 273, 296, false, 2)
+    // {
+
+    // }
+
+    protected override Item GetOneNew()
+    {
+        return new Kamera();
+    }
+}
